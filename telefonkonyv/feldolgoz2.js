6b588a591a0385eadcc540b550e3e481477d2bcc
KalacskaB <kalacska1010@gmail.com>
2021-03-09 09:55:08 +0100

    Módosítás

--- telefonkonyv/feldolgoz2.js
+++ telefonkonyv/feldolgoz2.js
@@ -3,6 +3,10 @@ $(function(){
     $("#beolvas").on("click",beolvas);
     $("#kuld").on("click",adBeir);
     $("article").delegate(".torol","click", adTorol);
+    $("article").delegate(".szerkeszt","click", adszerkeszt);
+    $("#megse").on("click",megse);
+    $("#modosit").on("click",modosit);
+    
     
 });
 
@@ -13,8 +17,10 @@ function kiir(){
         var nev = telefonkonyvem[i].nev;
         var tel = telefonkonyvem[i].tel;
         var kep = telefonkonyvem[i].kep;
-        var elem = "<div><h2>"+nev+"</h2><p>"+tel+"</p><p>"+tel+"</p><button class='torol' id='"+ID+"'>Törlés</button></div>";
-            $("article").append(elem);
+        var elem = "<div><h2>"+nev+"</h2><p>"+tel+"</p><p>"+tel+"</p>\n\
+        <button class='torol' id='"+ID+"'>Törlés</button>\n\
+        <button class='szerkeszt' id='"+i+"'>Szerkeszt</button></div>";
+        $("article").append(elem);
     }
     
 }
@@ -72,6 +78,46 @@ function adTorol(){
         },
         error: function () {
             alert("Hiba az adatok törlésekor!");
+        }
+    });
+}
+
+function adszerkeszt(){
+    console.log("Módosít");
+    
+    $(".szerkesztes").removeClass("elrejt");
+    
+    var index = $(this).attr("id");
+    console.log(index);
+    
+    $("#id2").val(telefonkonyvem[index].ID);
+    $("#nev2").val(telefonkonyvem[index].nev);
+    $("#tel2").val(telefonkonyvem[index].tel);
+    $("#kep2").val(telefonkonyvem[index].kep);
+}
+
+function megse(){
+      $(".szerkesztes").addClass("elrejt");
+}
+
+function modosit(){
+    var editszemely = {
+        ID: $("#id2").val(),
+        nev: $("#nev2").val(),
+        tel:  $("#tel2").val(),
+        kep: $("#kep2").val()
+    };
+    console.log("Módosít");
+    console.log(editszemely);
+    $.ajax({
+        type: "PUT",
+        url: "modosit.php",
+        data: editszemely,
+        success: function (){
+            beolvas();
+        },
+        error: function () {
+            alert("Hiba az adatok módosításakor!");
         }
     });
 }
--- telefonkonyv/index.php
+++ telefonkonyv/index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -25,15 +21,32 @@ and open the template in the editor.
             </nav>
 
             <section>
-                <p for="nev">név:</p>
-                <input type="text" id="nev" name="nev" value="Valaki Vagyok">
-                <p for="tel">tel:</p>
-                <input type="text" id="tel" name="tel" value="03-30-1234-567">
-                <p for="kep">kép:</p>
-                <input type="text" id="kep" name="kep">
-                <br>
-                <button id="kuld">Küld</button>
-
+                <form class="urlap">
+                    <label for="nev">név:</label>
+                    <input type="text" id="nev" name="nev" value="Valaki Vagyok">
+                    <label for="tel">tel:</label>
+                    <input type="text" id="tel" name="tel" value="03-30-1234-567">
+                    <label for="kep">kép:</label>
+                    <input type="text" id="kep" name="kep">
+                    <br>
+                    <input type="button" id="kuld" value="kuld" novalidate></input>
+                </form>
+            </section>
+            <section>
+                <form class="urlap szerkesztes elrejt">
+                    <label for="id2">ID:</label>
+                     <input type="text" id="id2" name="id2">
+                    <label for="nev2">név:</label>
+                    <input type="text" id="nev2" name="nev2" >
+                    <label for="tel2">tel:</label>
+                    <input type="text" id="tel2" name="tel2" >
+                    <label for="kep2">kép:</label>
+                    <input type="text" id="kep2" name="kep2">
+                    <br>
+                    <input type="button" id="modosit" value="Módosít" novalidate></input>
+                    <br>
+                    <input type="button" id="megse" value="Mégse" novalidate></input>
+                </form>
             </section>
             <article>
 
